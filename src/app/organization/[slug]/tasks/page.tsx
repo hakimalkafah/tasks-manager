2134c74899e77884e2609f438c3e833df906c829
hakimalkafah <hakimalkafah@gmail.com>
2025-08-28 23:32:19 -0400

    fix?

--- src/app/organization/[slug]/tasks/page.tsx
+++ src/app/organization/[slug]/tasks/page.tsx
@@ -106,7 +106,7 @@ export default function OrganizationTasksPage() {
       </header>
 
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        {/* Create Tasks Form */}
+        {/* Create Task Form */}
         {showCreateForm && (
           <Card className="mb-6">
             <CardHeader>
